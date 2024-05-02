c619bd0a0518f4bdafb9577a1c4ae76ba64935ba
Siddharth Garimella <gsidsid@gmail.com>
2024-05-01 20:27:02 -0700

    update

--- src/rulebricks/__init__.py
+++ src/rulebricks/__init__.py
@@ -1,23 +1,11 @@
 from typing import Optional
 from .client import RulebricksApi, AsyncRulebricksApi
 from .errors import BadRequestError, InternalServerError
-from .resources import (
-    ListResponseItem,
-    ListResponseItemRequestSchemaItem,
-    ListResponseItemResponseSchemaItem,
-    UsageResponse
-)
-
-# rulebricks/__init__.py
-
-import typing
-from .client import RulebricksApi, AsyncRulebricksApi
 from .resources.rules.client import RulesClient, AsyncRulesClient
 from .resources.flows.client import FlowsClient, AsyncFlowsClient
 
-# Configuration to hold API details
 class Config:
-    api_key: typing.Optional[str] = None
+    api_key: Optional[str] = None
     base_url: str = "https://rulebricks.com"
     timeout: float = 60
 
@@ -33,10 +21,9 @@ def set_timeout(timeout: float) -> None:
     Config.timeout = timeout
     APIManager.reset_instances()
 
-# API Manager for handling API instances
 class APIManager:
-    _instance: typing.Optional[RulebricksApi] = None
-    _async_instance: typing.Optional[AsyncRulebricksApi] = None
+    _instance: Optional[RulebricksApi] = None
+    _async_instance: Optional[AsyncRulebricksApi] = None
 
     @staticmethod
     def reset_instances():
@@ -53,7 +40,7 @@ class APIManager:
                 api_key=Config.api_key,
                 timeout=Config.timeout
             )
-        assert APIManager._instance is not None, "Instance of RulebricksApi should not be None"
+        assert APIManager._instance is not None, "Instance of AsyncRulebricksApi should not be None"
         return APIManager._instance
 
     @staticmethod
@@ -69,23 +56,19 @@ class APIManager:
         assert APIManager._async_instance is not None, "Instance of AsyncRulebricksApi should not be None"
         return APIManager._async_instance
 
-# Default API access through the root of the module
-def __getattr__(name: str):
-    api = APIManager.get_api()
-    if hasattr(api, name):
-        return getattr(api, name)
-    raise AttributeError(f"'RulebricksApi' object has no attribute '{name}'")
+# Synchronous API
+rules = APIManager.get_api().rules
+flows = APIManager.get_api().flows
 
-# Asynchronous API access
+# Asynchronous API
 class AsyncAPI:
-    def __getattr__(self, name: str):
-        async def async_method(*args, **kwargs):
-            api = await APIManager.get_async_api()
-            if hasattr(api, name):
-                func = getattr(api, name)
-                return await func(*args, **kwargs)
-            raise AttributeError(f"'AsyncRulebricksApi' object has no attribute '{name}'")
-        return async_method
+    @property
+    async def rules(self) -> AsyncRulesClient:
+        return (await APIManager.get_async_api()).rules
+
+    @property
+    async def flows(self) -> AsyncFlowsClient:
+        return (await APIManager.get_async_api()).flows
 
 async_api = AsyncAPI()
 
@@ -99,4 +82,7 @@ __all__ = [
     "async_api",
     "set_api_key",
     "set_instance_url",
+    "set_timeout",
+    "rules",
+    "flows",
 ]
